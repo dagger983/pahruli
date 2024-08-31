9786aeef765b1d36622d1338959093c0709e8288
dagger983 <kmvignesh1460@gmail.com>
2024-08-31 18:37:48 +0530

    first commit

--- src/components/Home/Navbar/PCNavbar.jsx
+++ src/components/Home/Navbar/PCNavbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState,useEffect } from 'react';
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
-const PCNavbar = ({ onCartClick}) => {
+const PCNavbar = ({ onCartClick }) => {
   const [username, setUsername] = useState(null);
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
     if (user && user.username) {
       setUsername(user.username);
     }
@@ -19,26 +19,38 @@ const PCNavbar = ({ onCartClick}) => {
           </div>
           <div className="pahruli-navbar3">
             <div className="contact-num">
-              <p>Contact : 9791522333</p>
+              <a href="tel:+919791522333">
+                <p>Contact :</p>
+              </a>
             </div>
             <div className="cart-icon">
               <img
                 src="/shopping-bag-min.png"
                 alt="Cart"
-                style={{ cursor: 'pointer', height: '30px' }}
+                style={{ cursor: "pointer", height: "30px" }}
                 onClick={onCartClick}
                 aria-label="Open cart"
               />
             </div>
             <div className="pahruli-navbar2">
-              <Link to="/"><p>Home</p></Link>
-              <Link to="/products"><p>Products</p></Link>
-              <Link to="/about-us"><p>About Us</p></Link>
-              <Link to="/contact"><p>Contact</p></Link>
+              <Link to="/">
+                <p>Home</p>
+              </Link>
+              <Link to="/products">
+                <p>Products</p>
+              </Link>
+              <Link to="/about-us">
+                <p>About Us</p>
+              </Link>
+              <Link to="/contact">
+                <p>Contact</p>
+              </Link>
               {username ? (
                 <p>Welcome, {username}</p>
               ) : (
-                <Link to="/login"><p>Login</p></Link>
+                <Link to="/login">
+                  <p>Login</p>
+                </Link>
               )}
             </div>
           </div>
